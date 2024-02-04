60d8a03ede2b68ced947e4968a97dc5de3b79285
Ergo <ergo.raidma@tktk.ee>
2024-02-04 10:47:36 +0200

    add week2 code

---
+++ userAgeValidation1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+//rakendus küsib kasutajal sisestada tema vanus
+//kui kasutaja vanus on väiksem kui 13, siis konsoolis kuvatakse:
+//you are too young to use Instagram
+//muul juhul
+//konsoolis kuvatakse "Welcome to Instagram"
+
+Console.WriteLine("Enter your age:");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse resault {isAgeNumber}. User is {userAgeNum} years old.");
+
+if (isAgeNumber) 
+{
+if (userAgeNum >=13)
+    {
+        Console.WriteLine("Welcome to Instagram.");
+    }
+    else 
+    {
+        Console.WriteLine("You are too young to use Instagram.");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age.");
+}
